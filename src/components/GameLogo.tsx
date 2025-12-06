06155ded1cbd185135e773f4017fa5d353013ff3
Matthieu <perdonium@gmail.com>
2025-12-06 14:28:18 +0100

    Fix game logos

--- src/components/GameLogo.tsx
+++ src/components/GameLogo.tsx
@@ -5,7 +5,7 @@ function GameLogo({name, additionalClass}:{name:string, additionalClass:string})
     return (
         <>
             <img
-                src={`/game-logos/${name}_EN.png`} alt={name}
+                src={`./game-logos/${name}_EN.png`} alt={name}
                 className={cn("w-18 md:w-[120px]", additionalClass)}/>
         </>
     )
